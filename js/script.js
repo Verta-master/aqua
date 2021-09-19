17612bbbed48c861cb047dae4d6e458350abf306
Verta-master <vertyagina@gmail.com>
2021-09-19 22:38:52 +0600

    Promo

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
-jQuery(function($) {
-	"use strict";
-
 //Mobile menu
 $('.menu__btn').click(function() {
-  $('.menu').toggleClass('menu--opened');
+  $(this).toggleClass('menu__btn--close');
   $('.menu__list').slideToggle();
 })
-
-if (window.innerWidth < 766) {
-  $('.menu__link').click(function() {
-    $('.menu__list').slideToggle();
-    $('.menu').removeClass('menu--opened');
-  })
-}
-
-//Scroll to top
-$(window).scroll(function(){
-  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-    $(".btn-top").fadeIn();
-  } else {
-    $(".btn-top").fadeOut();
-  };
-});
-
-$(".btn-top").click(function () {
-  $("body,html").animate({
-    scrollTop: 0
-  }, 400);
-  return false;
-});
 
 //Scroll to menu anchor
 $(document).ready(function () {
@@ -67,4 +41,3 @@ function onScroll(event){
       }
   });
 };
-});
