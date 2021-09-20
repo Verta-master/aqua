4fc8559c710579db84800ea60e48c0def1a96ba2
Verta-master <vertyagina@gmail.com>
2021-09-20 20:23:03 +0600

    Adaptive

--- js/script.js
+++ js/script.js
@@ -4,11 +4,27 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 });
 
+if (window.innerWidth < 1270) {
+  $('.menu__link').click(function() {
+    $('.menu__list').slideUp();
+    $('.menu__btn').removeClass('menu__btn--close');
+  })
+}
+
 //Read more
 $('.pumping__more').click(function() {
   event.preventDefault();
   $(this).hide();
   $('.pumping__text').slideToggle();
+});
+
+//Header scroll
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $('.header').addClass('header--scroll');
+  } else {
+    $('.header').removeClass('header--scroll');
+  };
 });
 
 //Scroll to menu anchor
